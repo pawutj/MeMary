c88ee345ff0e933c5cef2ada9132825e28f7fbf8
pawut jingjit <pawut.j@gmail.com>
2023-11-22 18:23:57 +0700

    Add new Decode

--- decode.js
+++ decode.js
@@ -1,8 +1,8 @@
 
-const decode = s=> {
+const encode = s=> {
 
-const a = "abcdefghijklmnopqrstuvwxyz"
-const b=  "qwertyuiopasdfghjklzxcvbnm"
+const a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const b=  "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
 
     if(a.indexOf(s) >=0){
         return b[a.indexOf(s)]
@@ -11,9 +11,42 @@ const b=  "qwertyuiopasdfghjklzxcvbnm"
 }
 
 const d = "if we're gonna live, we'll live together, but if we're gonna die, i'd rather die with you."
+const d2 = `
+A constellation is an area on the celestial sphere in which a group of visible stars forms a perceived pattern or outline
+, typically representing an animal, mythological subject, or inanimate object.[1]
+
+The origins of the earliest constellations likely go back to prehistory. 
+People used them to relate stories of their beliefs, experiences, creation, or mythology. 
+Different cultures and countries invented their own constellations, some of which lasted into the early 20th century before today's constellations were internationally recognized. 
+The recognition of constellations has changed significantly over time. Many changed in size or shape. 
+Some became popular, only to drop into obscurity. Some were limited to a single culture or nation. 
+Naming constellations also helped astronomers and navigators identify stars more easily.[2]`
+
+
+
 let temp = ""
-for(var i=0;i<d.length;i++){
-    temp+=decode(d[i])
+for(var i=0;i<d2.length;i++){
+    temp+=encode(d2[i])
+}
+console.log("______________________________________________")
+console.log(temp)
+
+
+const decode = s=> {
+
+    const a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const b=  "kx*mcnoph*rs*yi*adlegwruftKX*MCNOPH*RS*YI*ADLEGWRUFT"
+    
+        if(a.indexOf(s) >=0){
+            return b[a.indexOf(s)]
+        } 
+        return s
+    }
+
+let temp2 =""
+
+for(var i=0;i<d2.length;i++){
+    temp2+=decode(temp[i])
 }
 
-console.log(temp)
+console.log(temp2)
