75aee85806fe288049e46739e0fa10cacab2057a
pawut jingjit <pawut.j@gmail.com>
2023-12-16 18:37:17 +0700

    add

--- game/trans.py
+++ game/trans.py
@@ -1,4 +1,5 @@
 en = []
+th = []
 with open( "./trans/0_eng.txt" , encoding="utf-8") as f:
     content = f.read()
     lines  = content.split("\n")
@@ -11,5 +12,17 @@ with open( "./trans/0_eng.txt" , encoding="utf-8") as f:
             _s = s.split(":")[1].strip()
         print(_s)
         en += [_s]
-    
-    print(len(en))
+
+with open( "./trans/0_th.txt" , encoding="utf-8") as f:
+    content = f.read()
+    lines  = content.split("\n")
+
+    for s in lines:
+        _s =s
+        if(s.strip() == ""):
+            continue
+        if( ":" in s):
+            _s = s.split(":")[1].strip()
+        print(_s)
+        th += [_s]
+    print(len(th))
